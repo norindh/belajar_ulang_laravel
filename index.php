7b9fdb3254a9da248148a32c63670062f6221bd4
norindh_ <claranor26@gmail.com>
2022-05-15 15:27:33 +0700

    Menambahkan file index.html

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <body>
+    <?php
+echo"<p><h1>Belajar Variabel Hands On</h1></p>";
+//String
+$namaMurid = "Kevin";
+//String
+$warnaMobil = "Merah";
+//Integer
+$umurCalvin= 25;
+//float
+$beratBadanCalvin = 57.2;
+//Boolean
+$isSmart = true;
+
+echo"<br>Nama Murid Universtitas A: $namaMurid </br>";
+echo"<br>Warna Mobil $namaMurid : $warnaMobil </br>";
+echo"<br>umur $namaMurid : $umurCalvin</br>";
+echo"<br> berat badan $namaMurid : $beratBadanCalvin</br>";
+
+
+?>
+    </body>
+</html>
+
+
